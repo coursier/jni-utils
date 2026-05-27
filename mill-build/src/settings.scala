0973c0bff25590a89171d3e6c1a0408f26dd0872
Alex Archambault <alexandre.archambault@gmail.com>
2026-05-27 15:11:45 +0200

    Tweaking

--- mill-build/src/settings.scala
+++ mill-build/src/settings.scala
@@ -3,8 +3,6 @@ package millbuild
 import mill.*, scalalib.*
 import mill.scalalib.publish.PublishInfo
 
-import scala.concurrent.duration.Duration
-
 trait GenerateHeaders extends JavaModule {
   def cDirectory = Task {
     moduleDir / "src" / "main" / "c"
@@ -217,45 +215,6 @@ trait JniUtilsPublishModule extends PublishModule with JniUtilsPublishVersion {
       Developer("alexarchambault", "Alex Archambault","https://github.com/alexarchambault")
     )
   )
-}
-
-def publishSonatypeImpl(
-  credentials: String,
-  pgpPassword: String,
-  data: Seq[PublishModule.PublishData],
-  timeout: Duration,
-  log: mill.api.Logger,
-  workspace: os.Path,
-  env: Map[String, String]
-): Unit = {
-
-  val artifacts = data.map {
-    case PublishModule.PublishData(a, s) =>
-      (s.map { case (p, f) => (p.path, f) }, a)
-  }
-
-  val isRelease = {
-    val versions = artifacts.map(_._2.version).toSet
-    val set = versions.map(!_.endsWith("-SNAPSHOT"))
-    assert(set.size == 1, s"Found both snapshot and non-snapshot versions: ${versions.toVector.sorted.mkString(", ")}")
-    set.head
-  }
-  val publisher = new publish.SonatypePublisher(
-               uri = "https://oss.sonatype.org/service/local",
-       snapshotUri = "https://oss.sonatype.org/content/repositories/snapshots",
-       credentials = credentials,
-            signed = true,
-           gpgArgs = Seq("--detach-sign", "--batch=true", "--yes", "--pinentry-mode", "loopback", "--passphrase", pgpPassword, "--armor", "--use-agent"),
-       readTimeout = timeout.toMillis.toInt,
-    connectTimeout = timeout.toMillis.toInt,
-               log = log,
-         workspace = workspace,
-               env = env,
-      awaitTimeout = timeout.toMillis.toInt,
-    stagingRelease = isRelease
-  )
-
-  publisher.publishAll(isRelease, artifacts*)
 }
 
 trait WithDllNameJava extends JavaModule {
